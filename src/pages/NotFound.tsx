544344483c38e88fcb9d873d2af52535e63351b5
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-16 15:22:23 +0200

    customizing pageNotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
+import { ArticleBtnArrow } from '../components/Icons/articleBtnArrow.js';
+import Button from '@mui/material/Button';
 import React from 'react';
 
 export default function NotFound() {
   return (
     <div style={{ margin: 15 }}>
       <h1 style={{ marginLeft: 0, marginBottom: 15 }}>Page not found</h1>
-      <Link to="/">Back to home page</Link>
+      <Button color="secondary" disabled={false} className="card__button">
+        <span style={{ marginRight: 6, display: 'block' }}>
+          <ArticleBtnArrow />
+        </span>
+        <Link to="/" className="link">
+          Back to homepage
+        </Link>
+      </Button>
     </div>
   );
 }
