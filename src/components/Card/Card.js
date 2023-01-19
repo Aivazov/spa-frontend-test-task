deb1809e265d648f27d791ffe5efec8a9e52eaff
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-19 02:02:04 +0200

    mapping array in the card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -23,62 +23,116 @@ const convertTime = (time) => {
 //   let regExp = new RegExp(keyword, 'gi');
 // };
 
-export default function MediaCard({
-  title,
-  description,
-  date,
-  image,
-  filterArr,
-  url,
-}) {
+export default function MediaCard({ cards }) {
   return (
-    <Card className="card">
-      <CardMedia
-        sx={{ height: 217 }}
-        // image="../components/Icons/NoImage.jpg"
-        image={image ? image : '../components/Icons/NoImage.jpg'}
-        title={title}
-      />
-      <CardContent className="card__body">
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          className="card__date"
-        >
-          <span className="card__date--span">
-            <CardDateIcon className="card__date--icon" />
-          </span>
-          {convertTime(date)}
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          className="card__title"
-        >
-          {/* {title && (
+    <>
+      {cards.map(
+        ({ title, url, description, publishedAt, urlToImage, content }) => (
+          <Card className="card" key={url}>
+            <CardMedia
+              sx={{ height: 217 }}
+              // image="../components/Icons/NoImage.jpg"
+              image={
+                urlToImage ? urlToImage : '../components/Icons/NoImage.jpg'
+              }
+              title={title}
+            />
+            <CardContent className="card__body">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="card__date"
+              >
+                <span className="card__date--span">
+                  <CardDateIcon className="card__date--icon" />
+                </span>
+                {convertTime(publishedAt)}
+              </Typography>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className="card__title"
+              >
+                {/* {title && (
             <Highlighter searchWords={filterArr} textToHighlight={title} />
           )} */}
-          {title}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          className="card__description"
-        >
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <NavLink key={url} to="/article" className="read-more__link">
-          <Button color="secondary" disabled={false} className="card__button">
-            Read more
-            <span className="card__button--span">
-              <CardBtnArrow />
-            </span>
-          </Button>
-        </NavLink>
-      </CardActions>
-    </Card>
+                {title}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="card__description"
+              >
+                {description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <NavLink key={url} to="/article" className="read-more__link">
+                <Button
+                  color="secondary"
+                  disabled={false}
+                  className="card__button"
+                >
+                  Read more
+                  <span className="card__button--span">
+                    <CardBtnArrow />
+                  </span>
+                </Button>
+              </NavLink>
+            </CardActions>
+          </Card>
+        )
+      )}
+    </>
+
+    // <Card className="card">
+    //   <CardMedia
+    //     sx={{ height: 217 }}
+    //     // image="../components/Icons/NoImage.jpg"
+    //     image={image ? image : '../components/Icons/NoImage.jpg'}
+    //     title={title}
+    //   />
+    //   <CardContent className="card__body">
+    //     <Typography
+    //       variant="body2"
+    //       color="text.secondary"
+    //       className="card__date"
+    //     >
+    //       <span className="card__date--span">
+    //         <CardDateIcon className="card__date--icon" />
+    //       </span>
+    //       {convertTime(date)}
+    //     </Typography>
+    //     <Typography
+    //       gutterBottom
+    //       variant="h5"
+    //       component="div"
+    //       className="card__title"
+    //     >
+    //       {/* {title && (
+    //         <Highlighter searchWords={filterArr} textToHighlight={title} />
+    //       )} */}
+    //       {title}
+    //     </Typography>
+    //     <Typography
+    //       variant="body2"
+    //       color="text.secondary"
+    //       className="card__description"
+    //     >
+    //       {description}
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions>
+    //     <NavLink key={url} to="/article" className="read-more__link">
+    //       <Button color="secondary" disabled={false} className="card__button">
+    //         Read more
+    //         <span className="card__button--span">
+    //           <CardBtnArrow />
+    //         </span>
+    //       </Button>
+    //     </NavLink>
+    //   </CardActions>
+    // </Card>
   );
 }
