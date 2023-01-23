c07b99e1221890b3c8f22dd58e4cd81508eaaad5
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-23 20:46:32 +0200

    commit

--- src/api/fakeAPI.tsx
+++ src/api/fakeAPI.tsx
@@ -80,6 +80,6 @@ export function getArticles() {
   return articles;
 }
 
-export const getProductById = (articleId) => {
+export const getProductById = (articleId: string) => {
   return articles.find((article) => article.id === articleId);
 };
