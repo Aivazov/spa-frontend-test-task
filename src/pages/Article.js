c523d78b1e1711569777681e9c8b37c6f5a1bb56
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-22 13:41:32 +0200

    commit

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { ArticleBtnArrow } from '../components/Icons/articleBtnArrow.js';
 import { getArticleById } from '../api/fetchArticles.js';
 import Button from '@mui/material/Button';
+import Main from '../components/Main';
+import NotFound from './NotFound.tsx';
 import '../App.css';
 import '../components/pages_styles/Article/Article.css';
 
@@ -51,46 +53,11 @@ export default function Article() {
   } else {
     // throw new Error('Mistake for the undefined of "articleData"');
     return (
-      <div style={{ marginLeft: 15}}>
-        <h1>Something went wrong...</h1>
+      <div style={{ marginLeft: 15 }}>
+        {/* <h1>Oops... Something went wrong</h1> */}
+        <Main />
+        {/* <NotFound /> */}
       </div>
     );
   }
-  // return (
-  //   <div>
-  //     <div className="article__container">
-  //       <img
-  //         src={articleData.urlToImage}
-  //         alt=""
-  //         className="article__image"
-  //         style={{ backgroundPosition: 'center', height: 245 }}
-  //       />
-  //       <div className="wrap article__body">
-  //         <div className="container">
-  //           <div>
-  //             <h1 className="article__title">{articleData.title}</h1>
-  //           </div>
-  //           <div className="wrap">
-  //             <p className="article__content">{articleData.description}</p>
-  //           </div>
-  //           <div className="wrap">
-  //             <p className="article__content">{articleData.description}</p>
-  //           </div>
-  //           <Link to="/" className="link">
-  //             <Button
-  //               color="secondary"
-  //               disabled={false}
-  //               className="card__button"
-  //             >
-  //               <span style={{ marginRight: 6, display: 'block' }}>
-  //                 <ArticleBtnArrow />
-  //               </span>
-  //               Back to homepage
-  //             </Button>
-  //           </Link>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
